bfbe7511398085aad019b74f7f5b7d0a4676ed23
allasultan31 <sultan.alla@phystech.edu>
2021-09-20 18:20:40 +0300

    lab3

---
+++ lab3/1.py
@@ -0,0 +1,17 @@
+from graph import *
+
+
+brushColor("yellow")
+circle(250, 250, 100)
+brushColor("red")
+circle(200, 230, 20)
+circle(300, 230, 16)
+brushColor("black")
+circle(200, 230, 10)
+circle(300, 230, 7)
+penSize(10)
+line(225,220,150,180)
+line(280,220,350,200)
+penSize(20)
+line(200,300,300,300)
+run()
